b4f0f1b5bc613dc3527abb1a92a5a3bdb03713ee
Nikolai Dvirskii <ndvirskiy@mail.ru>
2023-12-25 00:59:57 +0500

    add task 005

---
+++ Seminar 2/Example_005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+    while (n > 999)
+        n /= 10;
+    Console.WriteLine(n % 10);
+}
