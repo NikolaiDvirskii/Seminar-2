c037e03d0e988f94e4b5ec6f03398a8798cc8a77
Nikolai Dvirskii <ndvirskiy@mail.ru>
2024-03-07 17:55:48 +0500

    add Lesson2:Task_003

---
+++ Lesson_2/Task_003/Program.cs
@@ -0,0 +1,12 @@
+﻿// int n = 10;
+int[] array = { 2, 3, 5, 10, 22, 15, 30, 8, 7, 4 };
+int i = 0;
+while(i < array.Length)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write(array[i]);
+        Console.Write(" ");
+    }
+i++;
+}
