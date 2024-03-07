3515354f7768e026bc07736a2b8330258d6b70d5
Nikolai Dvirskii <ndvirskiy@mail.ru>
2024-03-07 16:23:56 +0500

    fix code

--- Lesson_2/Task_002/Program.cs
+++ Lesson_2/Task_002/Program.cs
@@ -5,9 +5,9 @@ int i = 0;
 while (i < n)
 
 {
-arr[i] = i + 1;
-// Console.Write(arr[i]);
-// Console.Write(' ');
-Console.Write($"{arr[i]}" + ' ');
-i = i + 1;
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
 }
