845b0e2a41237e6d01e35cc8da4609b12839ced7
Nikolai Dvirskii <ndvirskiy@mail.ru>
2024-03-07 12:16:47 +0500

    add Seminar2:Example_003

--- Seminar 2/Example_003/Program.cs
+++ Seminar 2/Example_003/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = new Random().Next(100, 1000);
+Console.WriteLine("Random = " + n);
+Console.WriteLine(n / 100 * 10 + n % 10);
