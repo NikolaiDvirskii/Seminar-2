fad2cc53d31ca844ea7415c5849ea3d42efb45b2
Nikolai Dvirskii <ndvirskiy@mail.ru>
2024-03-07 12:15:08 +0500

    add Seminar2:Example_002

--- Seminar 2/Example_002/Program.cs
+++ Seminar 2/Example_002/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n < 100 || n > 999)
+{
+    Console.Write("Error! Enter the other number: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int n2 = n / 10 % 10;
+int n3 = n % 10;
+int result = 1;
+// for (int i = 1; i <= n3; i++)
+// {
+//     result *= n2;
+// }
+// Console.WriteLine("Loop:" + result);
+Console.WriteLine(Math.Pow(n2, n3));
