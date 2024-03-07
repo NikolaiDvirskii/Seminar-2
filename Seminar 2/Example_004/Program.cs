4013e05268ba58d67b68568562635ae78a511ac7
Nikolai Dvirskii <ndvirskiy@mail.ru>
2024-03-07 12:20:45 +0500

    add Semirar2:Example_004

--- Seminar 2/Example_004/Program.cs
+++ Seminar 2/Example_004/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter a first number:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a second number:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1 % n2 == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine("Не кратно " + n1 % n2);
