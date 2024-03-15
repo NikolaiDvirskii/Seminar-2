e50ce20be81c631494444eb35207dbd34cba62c6
Nikolai Dvirskii <ndvirskiy@mail.ru>
2024-03-16 00:31:22 +0500

    add Task_01

---
+++ Seminar_3/Task_01/Program.cs
@@ -0,0 +1,18 @@
+﻿int num = 8;
+int[] array = new int[] { 1, 2, 3, 6, 10, 9 };
+bool flag = false; 
+foreach (int item in array)
+{
+    if(num==item)
+    {
+        flag = true;
+    }
+}
+if (flag)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
