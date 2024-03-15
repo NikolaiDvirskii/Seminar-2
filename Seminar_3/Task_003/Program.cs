5ea4e84585d6be0f613aabfe6527adf6fc750ab1
Nikolai Dvirskii <ndvirskiy@mail.ru>
2024-03-16 00:33:22 +0500

    add Task_003

---
+++ Seminar_3/Task_003/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] array1 = new int[] { 1, 2, 3, 6, 9 };
+int[] array2 = new int[array1.Length / 2];
+
+for (int i = 0; i < array1.Length / 2; i++)
+{
+    array2[i] = array1[i] * array1[array1.Length - 1 - i];
+    Console.Write(array2[i] + " ");
+}
